6849048a332d51aa022507d36b85483aa3170011
Jonelle Alleyne <r.jonellealleyne@gmail.com>
2020-05-20 11:46:51 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var foo = 33;
+
+if (foo == 12){
+  console.log('Whassup');
+} else if (foo == 33){
+  console.log('My Ninja!');
+}else{
+  console.log('Dueces');
+}
+ switch(foo){
+   case 12: 
+   console.log('Whassup');
+   break;
+   case 33: 
+   console.log('My Ninja!');
+   break;
+   default:
+   console.log('Dueces')
+ }
